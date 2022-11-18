0d1c473d577e9678a21234de7ac51a918529d7c2
Egor <ekre4etov@yandex.ru>
2022-11-19 02:26:20 +0300

    Layout of the authorization component

---
+++ src/components/UI/YForm.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="YForm">
+  <form>
+    <div class="form__box">
+      <label class="box__lable">Логин</label>
+      <input class="box__input">
+    </div>
+    <div class="form__box">
+      <label class="box__lable">Пароль</label>
+      <input class="box__input">
+    </div>
+    <div class="form__button">
+      <button type="submit" class="btn btn__login">Войти</button>
+    </div>
+
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "YForm"
+}
+</script>
+
+<style scoped>
+.form__box {
+  color: white;
+  display: flex;
+  flex-direction: column;
+
+}
+input.box__input {
+  margin-top: 10px;
+  background: rgba(255, 255, 255, 0.23);
+  border: 2px solid rgba(255, 255, 255, 0.38);
+  border-radius: 14px;
+  color: white;
+  align-self: center;
+  height: 3em;
+  width: 15rem;
+  padding: 18px 16px;
+  font-size: 18px;
+
+
+}
+input.box__input:focus {
+  background: rgba(255, 255, 255, 0.17);
+  border-radius: 14px;
+  color: white;
+  box-shadow: 2px 2px 20px rgb(221 126 255 / 35%);
+  border: 2px solid rgba(255, 255, 255, 0.24);
+}
+.box__lable{
+  margin-top: 15px;
+  font-size:18px;
+}
+.form__button{
+  margin-top: 40px;
+}
+.btn__login {
+  text-align: center;
+  align-self: center;
+  background: linear-gradient(200.42deg, #B275FF 13.57%, #DD7EFF 98.35%);
+  border-radius: 12px;
+  color: white;
+  width: 60%;
+  margin-left: 20%;
+  margin-right: 20%;
+  padding: 18px 16px;
+  font-size: 18px;
+}
+.btn__login:hover {
+  background: linear-gradient(200.42deg, #B275FF 13.57%, #C247ED 98.35%);
+  box-shadow: 0px 0px 20px rgba(221, 126, 255, 0.35);
+  border-radius: 12px;
+  color: white;
+
+}
+.btn__login:focus {
+  background: linear-gradient(200.42deg, #B275FF 13.57%, #DD7EFF 98.35%);
+  box-shadow: 0px 0px 20px rgba(221, 126, 255, 0.35);
+  border-radius: 12px;
+  color: white;
+
+}
+</style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -3,11 +3,15 @@ import YBlockCard from '@/components/UI/YBlockCard';
 import YLeftSideBar from '@/components/UI/YLeftSideBar';
 import YModal from '@/components/UI/YModal';
 import YTestCard from '@/components/UI/YTestCard';
+import YForm from "@/components/UI/YForm";
+
 
 export default [
   YButton,
   YBlockCard,
   YLeftSideBar,
   YModal,
-  YTestCard
+  YTestCard,
+  YForm,
+
 ]
