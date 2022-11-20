f4617d86e5b39f81d9a5b4f02ecd7c120cb2e93a
Mihail <mohonovproduction@yandex.ru>
2022-11-20 23:42:29 +0300

    add tag script to App.vue, add auth method to api

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -1,7 +1,27 @@
-export default {
-  async auth() {
-    return new Promise((resole, reject) => {
-      fetch
+import config from '@/api/index';
+
+const Admin = {}
+
+Admin.auth = async function(email, password) {
+  return new Promise((resolve, reject) => {
+    fetch(`${config.host}/auth`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+      },
+      body: {
+        email,
+        password
+      }
     })
-  }
+      .then(async res => {
+        resolve(await res.json())
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
 }
+
+export default Admin
