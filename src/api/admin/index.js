dfb5ebad7bba4f46f641aa6c088042a99517bd30
Mihail <mohonovproduction@yandex.ru>
2022-11-20 23:46:46 +0300

    add auth/code method

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -24,4 +24,25 @@ Admin.auth = async function(email, password) {
   })
 }
 
+Admin.verify = async function(code) {
+  return new Promise((resolve, reject) => {
+    fetch(`${config.host}/auth/code`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive'
+      },
+      body: {
+        code
+      }
+    })
+      .then(async res => {
+        resolve(await res.json())
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
+
 export default Admin
