bd7667b7dae82bf3631148dc236b2f43b5f30812
Mihail <mohonovproduction@yandex.ru>
2022-11-25 18:43:31 +0300

    remove + png

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -7,11 +7,13 @@ import YList from "@/components/UI/YLits";
 import YListItem from '@/components/UI/YListItem';
 import YCoolButton from '@/components/UI/YCoolButton';
 import YInput from '@/components/UI/YInput';
+import YIcon from '@/components/UI/YIcon';
 
 
 export default [
   YButton,
   YCoolButton,
+  YIcon,
 
   YInput,
 
