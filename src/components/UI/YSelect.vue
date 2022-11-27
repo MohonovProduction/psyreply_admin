8ac3e23f68287baedb71a548a35b586021af407d
Mihail <mohonovproduction@yandex.ru>
2022-11-27 16:25:42 +0300

    select style fix

--- src/components/UI/YSelect.vue
+++ src/components/UI/YSelect.vue
@@ -1,12 +1,12 @@
 <template>
   <article class="select">
-    <button class="button" @click="list.show = !list.show">
+    <button class="button select__button" @click="list.show = !list.show">
       {{activeSelect[0].name}}
-      <img src="@/assets/img/select_arrow.svg">
+      <img class="button__icon" src="@/assets/img/select_arrow.svg">
     </button>
     <ul v-if="list.show" class="select__list">
-      <li v-for="el in notActiveSelects" class="list__item">
-        <button @click="select(el.data)" class="button_active">{{el.name}}</button>
+      <li v-for="el in notActiveSelects">
+        <button @click="select(el.data)" class="list__item button_active">{{el.name}}</button>
       </li>
     </ul>
   </article>
@@ -62,40 +62,40 @@ export default {
 
 <style scoped>
 
-.button{
+.button {
   cursor: pointer;
   align-self: end;
   position: relative;
   color: var(--light);
   font-size: 1rem;
-
 }
-.button img{
-  width: 15px;
+.button__icon {
+  width: 1rem;
 }
 
+.select__button {
+  padding: .3rem 1rem;
+}
 .select__list{
   position: absolute;
   background: rgba(255, 255, 255, 0.18);
   box-shadow: -4px 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(15px);
-  border-radius: 14px;
-
+  border-radius: .85rem;
+  list-style: none;
+  overflow: hidden;
+  min-width: 10rem;
 }
 .list__item{
-  padding: 1rem 3rem ;
-  list-style: none;
-  border-radius: 14px 14px 0px 0px;
-
-}
-.list__item button{
-
+  width: 100%;
+  padding: 1rem;
   color: var(--light);
   font-size: 1rem;
+  text-align: left;
 }
+
 .list__item:hover{
   background: linear-gradient(200.42deg, #B275FF 13.57%, #DD7EFF 98.35%);
-
 }
 
 </style>
