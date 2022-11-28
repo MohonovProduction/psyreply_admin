f925c796d4131c4a1988a799bcbd37bd150a0611
Mihail <mohonovproduction@yandex.ru>
2022-11-28 04:05:02 +0300

    add close event before company create

--- src/components/Company/CreateCompany.vue
+++ src/components/Company/CreateCompany.vue
@@ -65,11 +65,12 @@ export default {
       } else {
         return this.errorMessage('Слишком короткоее название')
       }
-      if (blocks.length > 0) body.inputBlocks = blocks
+      if (blocks.length > 0) body.company.inputBlocks = blocks
       company.create('', body)
         .then(res => {
           console.log(res)
           alert('компания создана')
+          this.$emit('close')
         })
     },
     errorMessage(err) {
